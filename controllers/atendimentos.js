9414ff4f502b7b4140c9e21ae418004fb29321ab
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2020-12-08 06:49:53 -0400

    Enviar dados relativos ao pagamento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,5 +3,8 @@ module.exports = app => {
     app.get('/atendimentos', (req,res) => res.send('Você está na rota de atendimentos '+
     'e está realizando um GET')) 
 
-    app.post('/atendimentos', (req,res) => res.send('Você está na rota de atendimentos e está realizando POST'))
+    app.post('/atendimentos', (req,res) => {
+        console.log(req.body);
+        res.send('Você está na rota de atendimentos e está realizando POST')
+    })
 }
