15deadb7f48f571cf949a47bf0ec1a47e8587cbf
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2020-12-16 22:18:18 -0400

    Segurança ao receber dados externos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,8 +1,19 @@
 const fs = require('fs')
+const path = require('path')
 
-module.exports = (caminho, nomeDoArquivo,callbackImagemCriada) => {
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
-    fs.createReadStream(caminho)
-        .pipe(fs.createWriteStream(novoCaminho))
-        .on('finish', () => callbackImagemCriada(novoCaminho))
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+    
+
+    if (tipoEhValido === -1) {
+        console.log('Erro! Tipo inválido!');
+    } else {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(novoCaminho))
+    }
 }
